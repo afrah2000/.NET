095291c4a03c2ab1090c8a113564d0abc928476b
Afrah Thabassum <56061913+afrah2000@users.noreply.github.com>
2022-09-27 10:02:22 +0530

    Add files via upload

---
+++ cupcakes.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CupcakesComponent } from './cupcakes.component';
+
+describe('CupcakesComponent', () => {
+  let component: CupcakesComponent;
+  let fixture: ComponentFixture<CupcakesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CupcakesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CupcakesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
